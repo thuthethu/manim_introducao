c81714f610048a8b3e8e30fa753a6f9718f907b0
Matheus <matheusgugus@gmail.com>
2026-04-12 23:07:30 -0300

    nova cena de um quadrado e renomieie as coisas

--- code/veja.py
+++ code/veja.py
@@ -1,14 +1,20 @@
 # ruff: noqa: F403, F405
+from typing_extensions import runtime
 from manim import *
 
 
-class MostrarLogo(Scene):
+class ShowLogo(Scene):
     def construct(self):
         banner = ManimBanner()
         self.play(banner.create(run_time=2))
         self.play(banner.expand(run_time=2))
         self.wait(2)
         self.play(Unwrite(banner))
+
+
+# Função de criar a linha do meio para poder ser reutilizavel
+def center_line():
+    return Line(UP * 4, DOWN * 4)
 
 
 # Criar uma classe dos codigos que vão aparecer na cena, para poder se reutilizavel
@@ -40,11 +46,11 @@ class CodeBlock(Code):
         )
 
 
-class ShowStuff(Scene):
+class CreateCircle(Scene):
     def construct(self):
 
         # codigo que vai aparecer na cena
-        code_python = """\
+        code_python_circle = """\
             from manim import *
                 class CriarUmCirculo(Scene):
                     def construct(self):
@@ -54,19 +60,58 @@ class ShowStuff(Scene):
 
             """
         # estruturação do codigo, de como ele vai ficar estilizado na cena
-        code_block = Code(code_string=code_python)
+        code_block_circle = Code(code_string=code_python_circle)
 
         # Ajusatr o bloco de codigo para que tenha cantoas arrendodados
-        code_block.scale(0.8).to_edge(LEFT, buff=0.5)
+        code_block_circle.scale(0.8).to_edge(LEFT, buff=0.5)
 
         # Criar a linha que vai dividir a tela no meio
-        linha_do_meio = Line(UP * 4, DOWN * 4)
+        line = center_line()
 
         # codigo a ser exibido no lado direito
         circle = Circle()
         circle.to_edge(RIGHT, buff=3.0)
 
-        self.play(Create(linha_do_meio))
-        self.wait(2)
-        self.play(FadeIn(code_block, shift=LEFT), Create(circle))
+        # Colocando a animação que foi feta acima na tela
+        self.play(Create(line))
+        self.wait()
+        self.play(
+            Create(code_block_circle),
+            Create(circle),
+            run_time=3,
+            rate_func=smooth,
+        )
+        self.wait(4)
+
+
+class CreateSquare(Scene):
+    def construct(self):
+
+        # codigo que vai aparecer na cena
+        code_python_square = """\
+            from manim import *
+                class CriarUmQuadrado(Scene):
+                    def construct(self):
+                        square = Square()
+
+                        self.play(square)
+            """
+
+        code_block_square = Code(code_string=code_python_square)
+
+        code_block_square.scale(0.8).to_edge(LEFT, buff=0.5)
+
+        line = center_line()
+
+        square = Square()
+        square.to_edge(RIGHT, buff=3.0)
+
+        self.play(Create(line))
+        self.wait()
+        self.play(
+            Create(code_block_square),
+            Create(square),
+            run_time=3,
+            rate_func=smooth,
+        )
         self.wait(4)
